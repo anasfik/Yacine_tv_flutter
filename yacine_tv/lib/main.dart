764a7457d5884865d6dc0fea02bb4c961a80cdec
Gwhyyy <ffikhi.aanas@gmail.com>
2023-01-18 14:28:25 +0100

    [ Add ] added comments to the Core class

--- yacine_tv/lib/main.dart
+++ yacine_tv/lib/main.dart
@@ -2,18 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:yacine_tv/core/lang/en.dart';
 
-import 'config/general.dart';
 import 'core/core.dart';
-import 'data/repositories/categories/categories.dart';
-import 'data/repositories/event_match/event_matches.dart';
-import 'data/repositories/menu/menu.dart';
-import 'data/repositories/settings/settings.dart';
 import 'logic/bloc_providers.dart';
-import 'logic/bottom_navigation/bottom_navigation_cubit.dart';
-import 'logic/channels_categories_cubit/channels_categories_cubit.dart';
-import 'logic/drawer_cubit/drawer_cubit_cubit.dart';
-import 'logic/event_matches/event_matches_cubit.dart';
-import 'logic/settings_cubit/settings_cubit.dart';
 import 'presentation/config/theme.dart';
 import 'presentation/screens/bottom_navigation_screens_handler/bottom_navigation_screens_handler.dart';
 
